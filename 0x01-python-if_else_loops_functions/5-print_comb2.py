7483259488a91fc3197631876a3a457a83bd2963
CodingFlamez <Umukoroisreal3@gmail.com>
2022-06-28 22:56:48 +0000

    New Task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for x in range(10):
         if x == 9 and i == 9:
-            print(f"{i}{x}")
+            print("{}{}".format(i, x))
         else:
-            print(f"{i}{x}", end=', ')
+            print(f"{}{}".format(i, x), end=', ')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for x in range(10):
         if ( i < x):
             if (i == 8) and (x == 9):
-                print(f"{i}{x}")
+                print("{}{}".format(i, x))
             else:
-                print(f"{i}{x}", end=', ')
+                print("{}{}".format(i, x), end=', ')
