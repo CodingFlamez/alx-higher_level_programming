4a89af7b8dd94ccd889c9542ae73f7a0f83a6a9e
CodingFlamez <Umukoroisreal3@gmail.com>
2022-06-29 00:03:01 +0000

    New Task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in range(len(str)):
+        y = ord(str[x])
+        if y in range(97, 123):
+            y = y - 32
+        if x == (len(str) - 1):
+            print("{}".format(chr(y)))
+        else:
+            print("{}".format(chr(y)), end='')
+
+
+
