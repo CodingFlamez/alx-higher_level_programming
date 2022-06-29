d693cfd02e42461ba875d9fd610b907607721c56
CodingFlamez <Umukoroisreal3@gmail.com>
2022-06-29 01:29:03 +0000

    New Task

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range(1,101):
+    for x in range(1, 101):
         if (x % 3) and (x % 5):
             print("{}".format(x), end=' ')
         else:
@@ -10,4 +10,3 @@ def fizzbuzz():
             if (x % 5) == 0:
                 b += 'Buzz'
             print("{}".format(b), end=' ')
-
