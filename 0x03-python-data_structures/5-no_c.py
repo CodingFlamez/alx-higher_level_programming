44d99a55602bcd40119ef37ef156a347cd9aa66b
CodingFlamez <Umukoroisreal3@gmail.com>
2022-07-04 16:49:06 +0000

    New Task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for x in my_string:
-        if x in 'cC':
+    a = len(my_string)
+    y = ""
+    for x in range(a):
+        if my_string[x] in 'cC':
             continue
         else:
-            print("{}".format(x), end='')
-    print()
+            y += my_string[x]
+    return y
