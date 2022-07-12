ffe4410f31f85ebc3bdacf9bf17d359ce00fcafb
CodingFlamez <Umukoroisreal3@gmail.com>
2022-07-12 09:02:58 +0000

    New Task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,9 +3,11 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
     len_b = len(tuple_b)
-    if len_a < len_b:
-        tuple_a += (0,)
-    elif len_b < len_a:
-        tuple_b += (0,)
+    if len_a < 2:
+        for x in range(len_a, 2):
+            tuple_a += (0,)
+    if len_b < 2:
+        for x in range(len_b, 2):
+            tuple_b += (0,)
     a = [x + y for x, y in zip(tuple_a, tuple_b)]
     return tuple(a)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    a = 0
-    for x in my_list:
-        if x > a:
-            a = x
-    if my_list:
-        return a
-    else:
+    if my_list is False:
         return None
+    else:
+        a = my_list[0]
+        for x in my_list:
+            if x > a:
+                a = x
+        return a
